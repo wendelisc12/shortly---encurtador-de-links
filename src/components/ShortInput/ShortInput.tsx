4eff5cd40908403f120cf4620f5d42386420d16e
Wendel <wendel.isaac12@gmail.com>
2024-08-12 12:46:48 -0300

    feat: usando função da api para fazer post em url

--- src/components/ShortInput/ShortInput.tsx
+++ src/components/ShortInput/ShortInput.tsx
@@ -1,17 +1,35 @@
 import { useState } from "react";
 import Button from "../Button/Button";
 import { Container, ErrorText, FloatContainer, Input, InputContainer } from "./style";
+import { useDispatch } from "react-redux";
+import { adicionarLinks } from "../../store/reducers/Link";
+import { useShortenUrlMutation } from "../../service/api";
+import { PulseLoader } from "react-spinners";
+
 
 const ShortInput = () => {
+
+    const dispatch = useDispatch()
+
+    const [shortenUrl, {isLoading}] = useShortenUrlMutation()
+                
 
     const[erro, setErro] = useState('')
     const[link, setLink] = useState('')
 
-    const handleLink = () => {
+    const handleLink = async () => {
         if(link.length > 0){
             try{
                 const url = new URL(link)
-                console.log('url valida' + url.href)
+                const response = await shortenUrl(url.toString())
+                const tinyUrl = response.data?.data.tiny_url
+                
+                if(tinyUrl){
+                    dispatch(adicionarLinks({linkOrigin: url.toString(), linkEncurtado: tinyUrl.toString()})) 
+                }else{
+                    alert("ocorreu um erro ao encurtar o link")
+                }
+                
             }catch{
                 setErro('invalido')
             }
@@ -28,7 +46,7 @@ const ShortInput = () => {
                         <Input type="text" placeholder="Shorten a link here" erro={erro} onFocus={()=>{setErro('')}} onChange={e => setLink(e.target.value)}/>
                         <ErrorText>{erro === 'invalido' ? 'Please add a valid link' : erro === 'vazio' ? 'please add a link' : ''}</ErrorText>
                     </InputContainer>
-                    <Button radius={10} width={170} height={50} onClick={handleLink}>Shorten It!</Button>
+                    <Button radius={10} width={170} height={50} onClick={handleLink}>{isLoading ? <PulseLoader color="#fff" size={10}/> : 'Shorten it!'}</Button>
                 </FloatContainer>
             </Container>
         </>
